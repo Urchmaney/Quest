68533f57af01e894a05b4d36aba1f33f381c50c0
Unegbu Kingsley <kingsobino@gmail.com>
2024-09-04 18:51:24 +0100

    adjust home page

--- ui/src/pages/public/home.tsx
+++ ui/src/pages/public/home.tsx
@@ -10,17 +10,17 @@ export const Home = () => {
                 </div>
                 <div className="flex gap-5 items-center">
                     <button className="bg-transparent underline text-base" type="button"><a className="text-base font-semibold">Login</a></button>
-                    <button className="bg-secondary text-white px-8 py-2 text-base rounded-sm" type="button">Register</button>
+                    <button className="bg-primary text-white px-8 py-2 text-base rounded-sm" type="button">Register</button>
                 </div>
             </header>
             <main className="flex flex-col justify-between pt-20 h-[calc(100vh-90px)]">
                 <div className="text-center flex flex-col items-center gap-10 h-[50%] justify-center">
-                    <h1 className="text-3xl md:text-6xl font-bold text-wrap">HOST HACKATHON</h1>
-                    <p className="max-w-md md:max-w-[60%] text-sm md:text-lg">
+                    <h1 className="text-7xl md:text-9xl font-bold text-wrap tracking-tighter">HOST HACKATHON</h1>
+                    <p className="max-w-md md:max-w-[60%] text-xl md:text-2xl">
                         All you need to run a successful hackathon such as participant registration, team matchmaking, event timeline, internal chat and more
                     </p>
                     <div>
-                        <button className="bg-secondary text-white p-3 px-5 rounded-md">
+                        <button className="bg-primary text-white p-3 px-14 rounded-md">
                             Host Hackathon
                         </button>
                     </div>
