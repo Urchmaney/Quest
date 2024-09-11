56f27a368cafa464cda9c9f9bd8d42917e7f5151
Unegbu Kingsley <kingsobino@gmail.com>
2024-09-11 15:34:24 +0100

    fix (UI) : hero section size

--- ui/src/pages/public/home.tsx
+++ ui/src/pages/public/home.tsx
@@ -1,11 +1,10 @@
 
-
 export const Home = () => {
     return (
 
-        <main className="flex flex-col justify-between pt-20 h-[calc(100vh-90px)]">
+        <main className="flex flex-col justify-between h-[calc(100vh-90px)]">
             <div className="text-center flex flex-col items-center gap-10 h-[50%] justify-center">
-                <h1 className="text-7xl md:text-9xl font-bold text-wrap tracking-tighter">HOST HACKATHON</h1>
+                <h1 className="text-4xl md:text-7xl xl:text-9xl font-bold text-wrap tracking-tighter">HOST HACKATHON</h1>
                 <p className="max-w-md md:max-w-[60%] text-xl md:text-2xl">
                     All you need to run a successful hackathon such as participant registration, team matchmaking, event timeline, internal chat and more
                 </p>
