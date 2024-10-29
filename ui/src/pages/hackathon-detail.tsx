f06b9ce2373c4b067715a563e000c1f3147e3a77
Unegbu Kingsley <kingsobino@gmail.com>
2024-10-29 17:02:26 +0100

    feat(ui) : style scrollbar

--- ui/src/pages/hackathon-detail.tsx
+++ ui/src/pages/hackathon-detail.tsx
@@ -22,14 +22,13 @@ export const HackathonDetail = () => {
   const result = converter.makeHtml(hackathonUI);
   
   return (
-
     <div className="relative">
       <div className="bg-primary h-28">
       </div>
       <div className="max-w-container mx-auto">
         <div className="h-[calc(100vh-200px)] bg-lightgray">
           <div className="container mx-auto h-full bg-lightgray">
-            <div className="bg-white h-[calc(100vh-100px)] relative top-[-100px] rounded-md overflow-y-auto ">
+            <div className="bg-white h-[calc(100vh-100px)] relative top-[-100px] rounded-md overflow-y-scroll ">
               <JsxParser components={{ MRLink }} jsx={result} />
             </div>
           </div>
